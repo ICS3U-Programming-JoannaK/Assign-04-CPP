4b9dd7cd57642d8df4f6a4f5c551936dd7ad5e13
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-05-06 06:11:39 +0000

    assign04.cpp

--- assign04.cpp
+++ assign04.cpp
@@ -76,7 +76,7 @@ int main() {
             float radians = angle * (M_PI / 180);
             // Calculates the tangent of the angle
             float tanNumber = tan(radians);
-            // Prints the tangent of the
+            // Prints the tangent of the angle in
             std::cout << angle << "\t\t" << std::fixed
             << std::setprecision(5) << tanNumber << std::endl;
         }
